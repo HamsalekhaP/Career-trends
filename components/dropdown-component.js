6fce997ca1309adf027f6b0753fa8f42328fd506
63033239+LicCheng@users.noreply.github.com <63033239+LicCheng@users.noreply.github.com>
2020-10-23 23:55:23 -0500

    Update dropdown-component.js

--- components/dropdown-component.js
+++ components/dropdown-component.js
@@ -1,16 +1,33 @@
 
 const React = require('react');
 import {Component} from 'react';
+import {MapComponent} from './map-component';
+import ReactMapGL from 'react-map-gl';
 
 class DropdownComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.options=props.listSchool    
-    this.state = this.options[0]
+    console.log("Hello");
+    this.options = props.listSchool;
+    
+    this.state = this.options[0];
+    
     this.props.updateProps({
-      returnCoordinates: [this.state['Latitude'],this.state['Longitude']]
+      returnCoordinates: [
+        this.state['Latitude'],
+        this.state['Longitude']
+      ]
     })
-    // console.log('state',this.state)
+    
+    this.viewport = {
+      width: 400,
+      height: 400,
+      latitude: this.props.returnCoordinates[0],
+      longitude: this.props.returnCoordinates[1],
+      zoom: 4,
+      mapboxApiAccessToken:"pk.eyJ1IjoibWF0aGlzb25pYW4iLCJhIjoiY2l5bTA5aWlnMDAwMDN1cGZ6Y3d4dGl6MSJ9.JZaRAfZOZfAnU2EAuybfsg"
+    };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -19,11 +36,16 @@ class DropdownComponent extends React.Component {
     this.setState({value: event.target.value});
     var selectedSchoolObj = this.options.filter(function(item) {
       return item['School Name'] == event.target.value
-    })    
+    });
+    console.log(selectedSchoolObj);
+    var latitude = selectedSchoolObj[0]['Latitude'];
+    var logitude = selectedSchoolObj[0]['Longitude'];
+
     this.props.updateProps({
-      returnCoordinates: [selectedSchoolObj[0]['Latitude'],selectedSchoolObj[0]['Longitude']]
+      returnCoordinates: [latitude, logitude]
     })
-    
+    this.viewport.latitude = latitude;
+    this.viewport.longitude = logitude;
   }
 
   handleSubmit(event) {
@@ -33,12 +55,18 @@ class DropdownComponent extends React.Component {
   render() {
     let modified_options = this.options.map(d => <option key={d['Index']}>{d['School Name']}</option>)
     return (
+      <div>
         <label>
           Pick a school:
           <select value={this.state.value} onChange={this.handleChange}>
             {modified_options}
           </select>
         </label>
+        <ReactMapGL
+          {...this.viewport} 
+          onViewportChange={(viewport) => this.setState({viewport})}
+        />
+      </div>
     );
   }
 }
